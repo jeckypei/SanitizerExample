0fd9d279f9fd04877bd8b3ba705da1f4bb3cf26a
jeckypei <jeckypei@163.com>
2022-05-23 09:04:42 +0800

    add MACRO isolation

--- poison.c
+++ poison.c
@@ -6,6 +6,11 @@ int main ( int argc, char argv[])
 {
 	char poison[1024] = {0};
 	printf("poison memory \n");
+#if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
+	printf("sanitizer enabled\n");
+#else
+	printf("sanitizer disabled\n");
+#endif
 	ASAN_POISON_MEMORY_REGION(poison, 1024);		
 	poison[1] = 1;
 	sleep(20000);
