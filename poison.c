c7b0b855ab92650db1761f4016c10b0e9043f4eb
jeckypei <jeckypei@163.com>
2022-05-23 09:07:41 +0800

    optimize printf

--- poison.c
+++ poison.c
@@ -5,13 +5,13 @@
 int main ( int argc, char argv[])
 {
 	char poison[1024] = {0};
-	printf("poison memory \n");
 #if __has_feature(address_sanitizer) || defined(__SANITIZE_ADDRESS__)
 	printf("sanitizer enabled\n");
 #else
 	printf("sanitizer disabled\n");
+	printf("poison memory ...\n");
 #endif
-	ASAN_POISON_MEMORY_REGION(poison, 1024);		
+	ASAN_POISON_MEMORY_REGION(poison, 1024); /*if SAN is not enabled, no operation*/
 	poison[1] = 1;
 	sleep(20000);
 }
